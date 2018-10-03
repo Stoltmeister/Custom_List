074d2e0fb4092130268e75f340018d4e761ff5e6
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-03 11:18:40 -0500

    Created and named count propert tests

--- CustomListUnitTestProject/CustomListUnitTests.cs
+++ CustomListUnitTestProject/CustomListUnitTests.cs
@@ -8,7 +8,7 @@ namespace CustomListUnitTestProject
     public class CustomListUnitTests
     {
 
-        // ***Add Tests***
+        // ***Add() Tests***
 
         [TestMethod]
         public void Add_IntParamForEmptyList_FirstIndexIsSameInt()
@@ -85,7 +85,7 @@ namespace CustomListUnitTestProject
             Assert.AreEqual(expected, actual);
         }
 
-        // ***Remove Tests***
+        // ***Remove() Tests***
 
         [TestMethod]
         public void Remove_IntIncludedInList_OtherIntIsOnlyOneRemaining()
@@ -172,5 +172,101 @@ namespace CustomListUnitTestProject
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        // *** Count Tests ***
+
+        [TestMethod]
+        public void Count_OneIntInList_CountEqualsOne()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int expected = 1;
+            int value = 22;
+            //Act
+            customList.Add(value);
+            int actual = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_EmptyList_CountEqualsZero()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int expected = 0;
+            //Act
+            int actual = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_AddAnotherValueToList_CountGoesUpOne()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 10;
+            int value1 = 22;
+            customList.Add(value);
+            int expected = customList.Count + 1;
+
+            //Act
+            customList.Add(value1);
+            int actual = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_RemoveAElementFromList_CountGoesDownOne()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 10;
+            int value1 = 22;
+            customList.Add(value);
+            customList.Add(value1);
+            int expected = customList.Count - 1;
+
+            //Act
+            customList.Remove(value1);
+            int actual = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_RemoveAElementThenAddAnother_CountIsTheSame()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 10;
+            int value1 = 22;
+            int value2 = 33;
+            customList.Add(value);
+            customList.Add(value1);
+            int expected = customList.Count;
+
+            //Act
+            customList.Remove(value1);
+            customList.Add(value2);
+            int actual = customList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // *** Indexing Tests ***
+
+        // *** '-' Operator Overload Tests ***
+
+        // *** '+' Operator Overload Tests ***
+
+        // *** List Zip Tests ***
     }
 }
