b6fdb3be47b339548ed13cf1bc6c9590e5702445
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-03 09:53:54 -0500

    Created project, added initial tests

---
+++ Custom_List_Project/CustomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_List_Project
+{
+    class CustomList<T>
+    {
+        T[] items = new T[1];
+
+        public void Add()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public virtual void ToString()
+        {
+
+        }
+    }
+}
