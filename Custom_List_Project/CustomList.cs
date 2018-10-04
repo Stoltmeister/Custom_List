5dbd1556f16d526405b3aa2d2d6db2e70046b646
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-04 11:38:06 -0500

    Coded Zip function, '+' and '-' overloads (not tested)

--- Custom_List_Project/CustomList.cs
+++ Custom_List_Project/CustomList.cs
@@ -4,35 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Custom_List_Project 
 {
-    public class CustomList<T> : IEnumerable<T>
+    public class CustomList<T> where T : IComparable
     {
         T[] items = new T[1];
-        int count;        
+        int count;
 
-        public int Count
+        public System.Collections.IEnumerator GetEnumerator()
         {
-            get => count;
-        }
-        public void Add()
-        {
-
-        }
-
-        public void Remove()
-        {
-
-        }
-
-        public void SetCount()
-        {
-
-        }
-
-        public override string ToString()
-        {
-            return "";
+            for (int i = 0; i < this.count; i++)
+            {
+                T element = this[i];
+                yield return element;
+            }
         }
         // indexer
         public T this[int i]
@@ -40,35 +26,97 @@ namespace Custom_List_Project
             get { return this[i]; }
             set { this[i] = value; }
         }
-        public System.Collections.IEnumerator GetEnumerator(CustomList<T> list)
+         public int CompareTo(object list)
         {
-            for (int i = 0; i < list.count; i++)
-            {
-                T element = list[i];
-                yield return element;
-            }
+            return 0;
         }
+        public int Count
+        {
+            get => count;
+        }
+        public void Add(T element)
+        {
+
+        }
+
+        public void Remove(T element)
+        {
+
+        }
+
+        private void SetCount()
+        {           
+            foreach (T e in this)
+            {
+                count++;
+            }            
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }        
+        
         public static CustomList<T> operator + (CustomList<T> List1, CustomList<T> List2)
         {
             CustomList<T> newList = new CustomList<T>();
 
-            // TO DO
+            for(int i = 0; i < List1.Count; i++)
+            {
+                newList.Add(List1[i]);
+            }
+            for (int i = 0; i < List2.Count; i++)
+            {
+                newList.Add(List2[i]);
+            }
+
             return newList;
         }
 
-        public static CustomList<T> operator - (CustomList<T> List1, CustomList<T> List2)
+        public static CustomList<T> operator - (CustomList<T> List1, CustomList<T> List2) 
         {
             CustomList<T> newList = new CustomList<T>();
+            newList = List1;
 
-            // TO DO
+            for (int i = 0; i < newList.Count; i++)
+            {
+               for (int j = 0; j < List2.Count; j++)
+                {
+                    if (newList[i].CompareTo(List2[j]) == 0)
+                    {
+
+                    }
+                }
+                newList.Remove(newList[i]);
+            }            
+
             return newList;
         }
 
         public static CustomList<T> Zip(CustomList<T> List1, CustomList<T> List2)
         {
             CustomList<T> newList = new CustomList<T>();
+            int highestCount;
 
-            // TO DO
+            if (List1.Count > List2.Count)
+            {
+                highestCount = List1.Count;
+            }
+            else
+            {
+                highestCount = List2.Count;
+            }
+            for (int i = 0; i < highestCount; i++)
+            {
+                if (List1[i] != null)
+                {
+                    newList.Add(List1[i]);
+                }
+                if (List2[i] != null)
+                {
+                    newList.Add(List2[i]);
+                }
+            }
             return newList;
         }
 
