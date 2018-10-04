ead7a528243cf896fba290a3d14660beec40a11e
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-10-04 10:07:07 -0500

    Added indexer (not tested)

--- Custom_List_Project/CustomList.cs
+++ Custom_List_Project/CustomList.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Custom_List_Project
+namespace Custom_List_Project 
 {
-    public class CustomList<T>
+    public class CustomList<T> : IEnumerable<T>
     {
         T[] items = new T[1];
-        int count;
+        int count;        
 
         public int Count
         {
@@ -34,7 +34,20 @@ namespace Custom_List_Project
         {
             return "";
         }
-
+        // indexer
+        public T this[int i]
+        {
+            get { return this[i]; }
+            set { this[i] = value; }
+        }
+        public System.Collections.IEnumerator GetEnumerator(CustomList<T> list)
+        {
+            for (int i = 0; i < list.count; i++)
+            {
+                T element = list[i];
+                yield return element;
+            }
+        }
         public static CustomList<T> operator + (CustomList<T> List1, CustomList<T> List2)
         {
             CustomList<T> newList = new CustomList<T>();
